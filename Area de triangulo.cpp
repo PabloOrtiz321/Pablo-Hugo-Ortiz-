98dd095ab1b5ae152160a228d7e6412859874061
PabloOrtiz321 <110415257+PabloOrtiz321@users.noreply.github.com>
2022-08-07 10:51:02 -0400

    Add files via upload

---
+++ Area de triangulo.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+     void main ()  {
+        int b,h.a 
+cout<< "Base";
+cin>>b;
+cout<< "altura";
+cin>>h; 
+a=(b*h);
+cout<<"a";
+getch(); }
